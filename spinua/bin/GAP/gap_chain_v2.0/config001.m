1c77818b951b974d018dc785647dfcaf88437aa5
khalidtijani <eu.khalidtijani@gmail.com>
2017-10-06 21:48:57 +0200

    new file:   config000.m 	new file:   config001.m 	new file:   config002.m 	new file:   config003.m

---
+++ spinua/bin/GAP/gap_chain_v2.0/config001.m
@@ -0,0 +1,21 @@
+%-------------------------
+% USER PARAMETERS
+%-------------------------
+
+UserPrms.Method='FINE'; % 'FAST', 'STANDARD' or 'FINE'
+
+UserPrms.WorkingVelocityPrecision_mmy = 3.0;
+UserPrms.WorkingDEMErrorPrecision_m = 15;
+
+UserPrms.MaxDEMError_m = 60;
+UserPrms.MaxVelocity_mmy = 60;
+
+UserPrms.icvTH=NaN;
+UserPrms.iicTH=NaN;
+UserPrms.meanCohTH=0.45;
+UserPrms.deltaIIC_product=-0.05;
+
+UserPrms.maxNproc=100;
+
+UserPrms.dataset_diff_suffix='orig';
+% UserPrms.dataset_diff_suffix='det004';
