3177514e276cad241d1e7478c62b887f5c27732c
khalidtijani <eu.khalidtijani@gmail.com>
2018-03-20 11:13:34 +0100

    modified:   src/main/app-resources/spinua-step-1/application.xml         new file:   src/main/app-resources/spinua-step-1/bash/env/env_include.sh 	modified:   src/main/app-resources/spinua-step-1/bash/splitter/run 	new file:   src/main/app-resources/spinua-step-2/bash/env/env_include.sh 	modified:   src/main/app-resources/spinua-step-2/bash/slcdatadownload/run 	new file:   src/main/app-resources/spinua-step-3/bash/env/env_include.sh 	modified:   src/main/app-resources/spinua-step-3/bash/supermaster/run 	modified:   src/main/app-resources/spinua-step-4/application.xml 	modified:   src/main/app-resources/spinua-step-4/bash/dem/run 	new file:   src/main/app-resources/spinua-step-4/bash/env/env_include.sh 	new file:   src/main/app-resources/spinua-step-5.backup/application.xml 	renamed:    src/main/app-resources/spinua-step-5/bash/interfconfig/run -> src/main/app-resources/spinua-step-5.backup/bash/interfconfig/run 	modified:   src/main/app-resources/spinua-step-5/application.xml 	new file:   src/main/app-resources/spinua-step-5/bash/env/env_include.sh 	renamed:    src/main/app-resources/spinua-step-6/bash/gpt/custom.vmoptions -> src/main/app-resources/spinua-step-5/bash/gpt/custom.vmoptions 	renamed:    src/main/app-resources/spinua-step-6/bash/gpt/snap_include.sh -> src/main/app-resources/spinua-step-5/bash/gpt/snap_include.sh 	renamed:    src/main/app-resources/spinua-step-6/bash/interfgen/run -> src/main/app-resources/spinua-step-5/bash/interfgen/run 	modified:   src/main/app-resources/spinua-step-6/application.xml 	new file:   src/main/app-resources/spinua-step-6/bash/env/env_include.sh 	new file:   src/main/app-resources/spinua-step-6/bash/geocoding/run 	renamed:    target/resources/application.xml -> src/main/app-resources/spinua-step-7/application.xml 	new file:   src/main/app-resources/spinua-step-7/bash/env/env_include.sh 	renamed:    target/resources/pscandidate/run -> src/main/app-resources/spinua-step-7/bash/pscandidate/run 	deleted:    src/main/app-resources/spinua-step-8/.application.xml.swp 	deleted:    src/main/app-resources/spinua-step-8/application.xml 	deleted:    src/main/app-resources/spinua-step-8/bash/pscandidate/run 	modified:   target/resources/application.xml 	new file:   target/resources/bash/geocoding/.run.swp 	new file:   target/resources/geocoding/.run.swp 	renamed:    src/main/app-resources/spinua-step-7/bash/geocoding/run -> target/resources/geocoding/run 	new file:   target/rpm/dcs-cnr-issia-spinua-step-6/RPMS/noarch/dcs-cnr-issia-spinua-step-6-1.0.11-1.noarch.rpm 	renamed:    target/rpm/dcs-cnr-issia-spinua-step-8/SPECS/dcs-cnr-issia-spinua-step-8.spec -> target/rpm/dcs-cnr-issia-spinua-step-6/SPECS/dcs-cnr-issia-spinua-step-6.spec 	deleted:    target/rpm/dcs-cnr-issia-spinua-step-8/RPMS/noarch/dcs-cnr-issia-spinua-step-8-1.0.11-1.noarch.rpm

---
+++ src/main/app-resources/spinua-step-2/bash/env/env_include.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# source the ciop functions (e.g. ciop-log, ciop-getparam)
+source ${ciop_job_include}
+# set the environment variables to use SPINUA sw and Storage Path  
+export USER=khalidtijani
+export STORAGE=/data/input/storage2
+export SPINUA=/home/${USER}/dcs-cnr-issia-spinua/spinua/bin/GAP/gap_chain_v2.0
+export MAINSPINUA=/home/${USER}/dcs-cnr-issia-spinua/spinua
+
+export COMMADS=/home/${USER}/dcs-cnr-issia-spinua/spinua
+
+export PATH=/bin:/usr/local/bin:/usr/bin:$COMMADS/bin:${PATH}
+
+# set the environment variables to use MATLAB Runtime v91 release R2016b
+export MCR_ROOT=/opt/MCR_R2016b
+export LD_LIBRARY_PATH=${MCR_ROOT}/v91/runtime/glnxa64:${MCR_ROOT}/v91/bin/glnxa64:${MCR_ROOT}/v91/sys/os/glnxa64:${LD_LIBRARY_PATH}
+
