fbf236bee879a25ad851fb05dab9c31f19bd9b4b
khalidtijani <eu.khalidtijani@gmail.com>
2018-04-20 23:55:37 +0200

    modified:   spinua/bin/GAP/gap_chain_v2.0/GAP_DEM_preprocessing/GAP_DEM_preprocessing_PathSettings.txt 	modified:   spinua/bin/GAP/gap_chain_v2.0/GAP_configure_s1tbx/GAP_configure_s1tbx_PathSettings.txt 	modified:   spinua/bin/GAP/gap_chain_v2.0/GAP_run_s1tbx/GAP_run_s1tbx_PathSettings.txt 	modified:   src/main/app-resources/spinua-step-3/bash/supermaster/run 	modified:   src/main/app-resources/spinua-step-4/bash/dem/run 	modified:   src/main/app-resources/spinua-step-5/application.xml 	modified:   src/main/app-resources/spinua-step-5/bash/interfgen/run 	modified:   src/main/app-resources/spinua-step-5/gpt/snap_include.sh 	modified:   target/resources/README.md 	modified:   target/resources/application.xml 	deleted:    target/resources/supermaster/run 	deleted:    target/rpm/dcs-gap-spinua-step-3/RPMS/noarch/dcs-gap-spinua-step-3-1.0.11-1.noarch.rpm 	deleted:    target/rpm/dcs-gap-spinua-step-3/SPECS/dcs-gap-spinua-step-3.spec

--- src/main/app-resources/spinua-step-5/gpt/snap_include.sh
+++ src/main/app-resources/spinua-step-5/gpt/snap_include.sh
@@ -2,15 +2,15 @@
 
 
 
-export gpt_path_s1tbx_v1_1_1='/home/khalidtijani/dcs-cnr-issia-spinua/spinua/sw/snap_modif/bin/gpt';
-export gpt_path_snap_v3_0='/home/khalidtijani/dcs-cnr-issia-spinua/spinua/sw/snap_modif/bin/gpt';
-export gpt_path_snap_v4_0='/home/khalidtijani/dcs-cnr-issia-spinua/spinua/sw/snap_modif/bin/gpt';
-export gpt_path_snap_v5_0='/home/khalidtijani/dcs-cnr-issia-spinua/spinua/sw/snap_modif/bin/gpt';
+export gpt_path_s1tbx_v1_1_1='/home/khalidtijani/dcs-gap-spinua/spinua/sw/snap_modif/bin/gpt';
+export gpt_path_snap_v3_0='/home/khalidtijani/dcs-gap-spinua/spinua/sw/snap_modif/bin/gpt';
+export gpt_path_snap_v4_0='/home/khalidtijani/dcs-gap-spinua/spinua/sw/snap_modif/bin/gpt';
+export gpt_path_snap_v5_0='/home/khalidtijani/dcs-gap-spinua/spinua/sw/snap_modif/bin/gpt';
 
-export gptM_path_s1tbx_v1_1_1='/home/khalidtijani/dcs-cnr-issia-spinua/spinua/sw/snap_modif_master/bin/gpt';
-export gptM_path_snap_v3_0='/home/khalidtijani/dcs-cnr-issia-spinua/spinua/sw/snap_modif_master/bin/gpt';
-export gptM_path_snap_v4_0='/home/khalidtijani/dcs-cnr-issia-spinua/spinua/sw/snap_modif_master/bin/gpt';
-export gptM_path_snap_v5_0='/home/khalidtijani/dcs-cnr-issia-spinua/spinua/sw/snap_modif_master/bin/gpt';
+export gptM_path_s1tbx_v1_1_1='/home/khalidtijani/dcs-gap-spinua/spinua/sw/snap_modif_master/bin/gpt';
+export gptM_path_snap_v3_0='/home/khalidtijani/dcs-gap-spinua/spinua/sw/snap_modif_master/bin/gpt';
+export gptM_path_snap_v4_0='/home/khalidtijani/dcs-gap-spinua/spinua/sw/snap_modif_master/bin/gpt';
+export gptM_path_snap_v5_0='/home/khalidtijani/dcs-gap-spinua/spinua/sw/snap_modif_master/bin/gpt';
 
 export CACHE_SIZE=2048M
 
